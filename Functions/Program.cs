c92482c298d308514acfe9bc134374e224f0c2a1
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2023-09-13 14:15:29 +0200

    functions example done;

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,11 +1,22 @@
 ﻿void printHello()
 {
-  Console.WriteLine("Hello");
+ Console.WriteLine("Hello");
 }
 
 string getHello()
 {
   return "Hello";
+}
+
+void printGreeting(string name, int itercount = 1)
+{
+  int i = 0;
+  while (i < itercount)
+  {
+    Console.WriteLine($"Hello {name}");
+    i++; // i = i + 1;
+  }
+  Console.WriteLine("sdklfjls");
 }
 
 printHello();
@@ -15,3 +26,13 @@ var hello = getHello();
 hello = hello + " World!";
 
 Console.WriteLine(hello);
+
+Console.WriteLine("Nenne mir deinen Namen");
+var inputName = Console.ReadLine();
+
+printGreeting(inputName);
+
+
+//printGreeting(name: inputName, itercount: 10);
+//printGreeting(itercount: 42, name: "Olaf");
+//printGreeting(itercount: 42, name: inputName);
