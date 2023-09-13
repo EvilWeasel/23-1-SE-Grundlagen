a705585cf7601cdb5a49c2ca11764171b49196fe
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2023-09-13 15:08:43 +0200

    improved taschenrechner by implementing function for getting input

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -1,21 +1,32 @@
-﻿Console.WriteLine("Willkommen beim Taschenrechner 3000");
-bool checkParse1 = false;
-bool checkParse2 = false;
-int num1 = 0;
-int num2 = 0;
-while (!checkParse1 || !checkParse2)
+﻿int GetNumber()
 {
-  Console.WriteLine("Bitte gebe Zahl 1 an:");
-  string input1 = Console.ReadLine();
+  int num = 0;
+  bool checkParse = false;
+  do
+  {
+    Console.WriteLine("Gebe eine Zahl ein");
+    string input = Console.ReadLine();
+    checkParse = int.TryParse(input, out num);
 
-  Console.WriteLine("Bitte gebe Zahl 2 an:");
-  string input2 = Console.ReadLine();
-
-
-  // Konvertierung von Text zu Zahlen
-  checkParse1 = int.TryParse(input1, out num1);
-  checkParse2 = int.TryParse(input2, out num2);
+    if (checkParse == false)
+    {
+      Console.WriteLine("Ungültige Eingabe: Nur Ganzzahlen sind erlaubt");
+    }
+  } while (checkParse == false);
+  return num;
 }
+
+string GetOperation()
+{
+  // code...
+
+  return "+";
+}
+
+Console.WriteLine("Willkommen beim Taschenrechner 3000");
+var num1 = GetNumber();
+var num2 = GetNumber();
+var operation = GetOperation();
 var result = num1 + num2;
 Console.WriteLine($"{num1} + {num2} = {result}");
 
