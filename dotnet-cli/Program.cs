29588232fa3291f4b1ff4ac2b6a3967257ff92a1
Tobias Wehrle <38261180+EvilWeasel@users.noreply.github.com>
2023-09-18 12:21:51 +0200

    how to dotnet from the cli - added :)

---
+++ dotnet-cli/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Wie viele Zufallszahlen möchtest du?");
+
+var inputAmount = Console.ReadLine();
+
+
+var randomAmount = int.Parse(inputAmount);
+
+var rng = new Random();
+
+int[] randomNumbers = new int[randomAmount];
+
+for (int i = 0; i < randomNumbers.Length; i++)
+{
+    randomNumbers[i] = rng.Next(0, 69);
+}
+
+Console.WriteLine(string.Join(", ", randomNumbers));
