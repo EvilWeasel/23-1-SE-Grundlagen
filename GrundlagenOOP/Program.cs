24b87edadf9fff9f83a5b53467f34353953e6371
Tobias Wehrle <38261180+EvilWeasel@users.noreply.github.com>
2023-09-26 11:58:30 +0200

    Grundlagen zu Klassen und Objekten, Static, sowohl als auch Instanz

--- GrundlagenOOP/Program.cs
+++ GrundlagenOOP/Program.cs
@@ -5,8 +5,32 @@ internal class Program
   {
     Console.WriteLine("Hello, World!"); // Statische-Klasse
     Random rng = new Random(); // Instanziierbare-Klasse
-    Cat myCat = new Cat();
-    myCat.Miau();
-    Cat.Miau();
+    //Cat myCat = new Cat();
+    int x;
+    Cat y; // Cat ist unser eigens erstellter Datentyp
+    // myCat.Miau();
+    // myCat.Name = "Minka";
+    // myCat.Age = 10;
+    // myCat.Color = "Silver-Tabby";
+    // Cat myCoolCat = new Cat();
+    // myCoolCat.Color = "Brown";
+    // myCoolCat.Age = 12;
+    // myCoolCat.Name = "Tom";
+    // Console.WriteLine(myCat);
+    // Console.WriteLine(myCoolCat);
+    // myCoolCat.Name = "Steffan";
+    // Console.WriteLine(myCoolCat);
+
+    Cat jerry = new Cat("jerry", 8, "black");
+    Console.WriteLine(jerry);
+    jerry.Age += 1;
+
+    jerry = new Cat("minka", 12, "silver");
+
+    jerry.Miau();
+
+    Cat.Schnurren();
+    Console.WriteLine(jerry);
+    Console.ReadLine();
   }
 }
